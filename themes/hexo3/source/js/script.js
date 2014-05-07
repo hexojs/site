011a6f8ca9e0d87c06f52e888f037fe832564e4c
Tommy Chen <tommy351@gmail.com>
2014-05-08 00:08:24 +0800

    Add mobile menu to API pages

--- themes/hexo3/source/js/script.js
+++ themes/hexo3/source/js/script.js
@@ -1,4 +1,35 @@
 (function($){
+  // Create menu for mobile devices
+  var menuHtml = [],
+    lastTag = '';
+
+  menuHtml.push('<select id="page-mobile-menu">');
+  menuHtml.push('<option value="">Navigate...</option>');
+
+  $('#sidebar').children().each(function(){
+    var tag = this.tagName.toLowerCase(),
+      $this = $(this);
+
+    switch (tag){
+      case 'strong':
+        if (lastTag === 'a') menuHtml.push('</optgroup>');
+
+        menuHtml.push('<optgroup label="' + $this.html() + '">');
+        break;
+
+      case 'a':
+        menuHtml.push('<option value="' + $this.attr('href') + '">' + $this.html() + '</option>');
+        break;
+    }
+
+    lastTag = tag;
+  });
+
+  menuHtml.push('</optgroup>');
+  menuHtml.push('</select>');
+
+  $('#mobile-menu-wrap').html(menuHtml.join(''));
+
   $('#banner-getting-started-input').on('click', function(){
     $(this).select();
   });
