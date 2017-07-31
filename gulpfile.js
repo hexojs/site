18dc3331eaf6888972b88446555d9d84bb61ceb6
Tommy Chen <tommy351@gmail.com>
2017-08-01 02:21:14 +0800

    Use img srcset for retina images

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,90 @@
 'use strict';
 
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var cssnano = require('cssnano');
+var gulpIf = require('gulp-if');
+var gulpRev = require('gulp-rev');
+var gulpRevCollector = require('gulp-rev-collector');
+var gulpRevReplace = require('gulp-rev-replace');
+var gulpUglify = require('gulp-uglify');
+var gulpUniqueFiles = require('gulp-unique-files');
+var gulpUseRef = require('gulp-useref');
+var gulpCleanCSS = require('gulp-clean-css');
+var gulpResponsive = require('gulp-responsive');
+var gulpImgRetina = require('gulp-img-retina');
+var del = require('del');
 
 var dirs = {
   public: 'public',
   screenshots: 'public/build/screenshots'
 };
 
-gulp.task('useref', ['screenshot'], function(){
-  var assets = $.useref.assets({
+var retinaSuffix = {
+  1: '',
+  2: '@2x'
+};
+
+gulp.task('useref', ['screenshot'], function() {
+  var assets = gulpUseRef.assets({
     searchPath: 'public'
   });
 
   return gulp.src('public/**/*.html')
     .pipe(assets)
-    .pipe($.uniqueFiles())
-    .pipe($.if('*.css', $.postcss([
-      cssnano()
-    ])))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.rev())
+    .pipe(gulpUniqueFiles())
+    .pipe(gulpIf('*.js', gulpCleanCSS()))
+    .pipe(gulpIf('*.js', gulpUglify()))
+    .pipe(gulpRev())
     .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe($.revReplace({
+    .pipe(gulpUseRef())
+    .pipe(gulpRevReplace({
       prefix: '/'
     }))
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('screenshot:rev', function(){
+gulp.task('screenshot:clean', function() {
+  return del([dirs.screenshots + '/**/*']);
+});
+
+gulp.task('screenshot:rev', ['screenshot:clean'], function() {
   return gulp.src('public/themes/screenshots/*.png')
-    .pipe($.rev())
+    .pipe(gulpRev())
     .pipe(gulp.dest(dirs.screenshots))
-    .pipe($.rev.manifest())
+    .pipe(gulpRev.manifest())
     .pipe(gulp.dest(dirs.screenshots));
 });
 
-gulp.task('screenshot:resize', ['screenshot:rev'], function(){
-  return gulp.src(dirs.screenshots + '/*.png')
-    .pipe($.responsive({
-      '*.png': [
-        {
-          width: 400,
-          progressive: true
-        },
-        {
-          progressive: true,
-          rename: {
-            suffix: '@2x'
-          }
-        }
-      ]
-    }))
-    .pipe(gulp.dest(dirs.screenshots));
-});
-
-gulp.task('screenshot:revreplace', ['screenshot:rev'], function(){
+gulp.task('screenshot:revreplace', ['screenshot:rev'], function() {
   return gulp.src([dirs.screenshots + '/rev-manifest.json', 'public/themes/index.html'])
-    .pipe($.revCollector({
+    .pipe(gulpRevCollector({
       replaceReved: true,
       dirReplacements: {
         '/themes/screenshots': '/build/screenshots'
       }
     }))
+    .pipe(gulpImgRetina({
+      suffix: retinaSuffix
+    }))
     .pipe(gulp.dest('public/themes'));
+});
+
+gulp.task('screenshot:resize', ['screenshot:rev'], function() {
+  return gulp.src(dirs.screenshots + '/*.png')
+    .pipe(gulpResponsive({
+      '*.png': [
+        {
+          width: 400
+        },
+        {
+          rename: {
+            suffix: retinaSuffix[2]
+          }
+        }
+      ]
+    }, {
+      progressive: true
+    }))
+    .pipe(gulp.dest(dirs.screenshots));
 });
 
 gulp.task('screenshot', ['screenshot:rev', 'screenshot:resize', 'screenshot:revreplace']);
