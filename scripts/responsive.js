5ff79f32c2e6a2e29c68402137216c62d6f70fb3
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-06 16:52:20 +0100

    fix: update png path

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -3,14 +3,14 @@
 'use strict';
 
 const { createHash } = require('crypto');
-const { full_url_for, url_for } = require('hexo-util');
+const { url_for } = require('hexo-util');
 const sharp = require('sharp');
 
 async function responsive() {
   const { route } = this;
   const routeList = route.list();
   const pngFiles = routeList.filter((path) => path.startsWith('themes') && path.endsWith('.png'));
-  const pngToJpg = {};
+  const updatePng = {};
 
   await Promise.all(pngFiles.map((path) => {
     return new Promise((resolve, reject) => {
@@ -33,14 +33,16 @@ async function responsive() {
               .toBuffer();
 
             const pngHash = createHash('sha1').update(input).digest('hex').slice(0, 10);
+            const pngPath = path.replace(/\.png$/, `-${pngHash}.png`);
             const jpegHash = createHash('sha1').update(jpeg).digest('hex').slice(0, 10);
+            const jpegPath = path.replace(/\.png$/, `-${halfJpegHash}@2x.jpg`)
             const halfJpegHash = createHash('sha1').update(halfJpeg).digest('hex').slice(0, 10);
             const halfJpegPath = path.replace(/\.png$/, `-${jpegHash}.jpg`);
 
-            pngToJpg[url_for.call(this, path)] = url_for.call(this, halfJpegPath);
-            route.set(path.replace(/\.png$/, `-${pngHash}.png`), input);
+            updatePng[url_for.call(this, path)] = url_for.call(this, pngPath);
+            route.set(pngPath, input);
             route.set(halfJpegPath, halfJpeg);
-            resolve(route.set(path.replace(/\.png$/, `-${halfJpegHash}@2x.jpg`), jpeg));
+            resolve(route.set(jpegPath, jpeg));
           } catch (err) {
             reject(err);
           }
@@ -58,7 +60,7 @@ async function responsive() {
         if (assetData.length) {
           try {
             const result = assetData.join().replace(/src=['"](.*?)['"]/gi, (str, value) => str.replace(value, (img) => {
-              if (pngToJpg[img]) return pngToJpg[img];
+              if (updatePng[img]) return updatePng[img];
               return img;
             }));
             resolve(route.set(path, result));
