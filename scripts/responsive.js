b7b2a45e939b0d82d96c0eb7ae424bc80ea73c6b
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-07 02:09:39 +0100

    refactor: replace gulp-cheerio with String.replace()

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -33,16 +33,21 @@ async function responsive() {
               .toBuffer();
 
             const pngHash = createHash('sha1').update(input).digest('hex').slice(0, 10);
-            const pngPath = path.replace(/\.png$/, `-${pngHash}.png`);
             const jpegHash = createHash('sha1').update(jpeg).digest('hex').slice(0, 10);
-            const jpegPath = path.replace(/\.png$/, `-${halfJpegHash}@2x.jpg`)
             const halfJpegHash = createHash('sha1').update(halfJpeg).digest('hex').slice(0, 10);
-            const halfJpegPath = path.replace(/\.png$/, `-${jpegHash}.jpg`);
+            const pngPath = path.replace(/\.png$/, `-${pngHash}.png`);
+            const jpeg2xPath = path.replace(/\.png$/, `-${jpegHash}@2x.jpg`);
+            const jpegPath = path.replace(/\.png$/, `-${halfJpegHash}.jpg`);
+            const validPath = url_for.call(this, path);
+            const validPngPath = url_for.call(this, pngPath);
+            const validJpg2xPath = url_for.call(this, jpeg2xPath);
+            const validJpgPath = url_for.call(this, jpegPath);
 
-            updatePng[url_for.call(this, path)] = url_for.call(this, pngPath);
+            updatePng[validPath] = { 'png': validPngPath, 'jpg2x': validJpg2xPath, 'jpg': validJpgPath };
+
             route.set(pngPath, input);
-            route.set(halfJpegPath, halfJpeg);
-            resolve(route.set(jpegPath, jpeg));
+            route.set(jpegPath, halfJpeg);
+            resolve(route.set(jpeg2xPath, jpeg));
           } catch (err) {
             reject(err);
           }
@@ -59,10 +64,26 @@ async function responsive() {
       assetPath.on('end', async() => {
         if (assetData.length) {
           try {
-            const result = assetData.join().replace(/src=['"](.*?)['"]/gi, (str, value) => str.replace(value, (img) => {
-              if (updatePng[img]) return updatePng[img];
-              return img;
-            }));
+            const result = assetData.join().replace(/<img (src|data-src)=['"](.*?)['"](.*?)>/gi, (imgTag, attr, value, alt) => {
+              imgTag = imgTag.replace(value, (img) => {
+                if (updatePng[img]) return updatePng[img].png;
+                return img;
+              });
+
+              if (attr === 'data-src') {
+                const png = updatePng[value] ? updatePng[value].png : value;
+                const jpg = updatePng[value] ? updatePng[value].jpg : value;
+                const jpg2x = updatePng[value] ? updatePng[value].jpg2x : value;
+
+                imgTag = imgTag.replace(alt, (postAlt) => {
+                  postAlt += ` data-srcset="${jpg}, ${jpg2x} 2x" data-org="${png}" sizes="360px" srcset="${jpg}, ${jpg2x} 2x" src="${jpg}"`;
+                  return postAlt;
+                });
+              }
+
+              return imgTag;
+            });
+
             resolve(route.set(path, result));
           } catch (err) {
             reject(err);
