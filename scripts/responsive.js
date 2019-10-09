b743509ce86827b478f3e2e461059e044c028588
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-09 10:43:30 +0100

    style: eslint

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -31,8 +31,8 @@ async function responsive() {
               .resize({ width: Math.round(metadata.width / 2) })
               .toBuffer();
 
-            const jpeg2xPath = path.replace(/\.png$/, `@2x.jpg`);
-            const jpegPath = path.replace(/\.png$/, `.jpg`);
+            const jpeg2xPath = path.replace(/\.png$/, '@2x.jpg');
+            const jpegPath = path.replace(/\.png$/, '.jpg');
             const validPath = url_for.call(this, path);
             const validJpg2xPath = url_for.call(this, jpeg2xPath);
             const validJpgPath = url_for.call(this, jpegPath);
@@ -59,14 +59,14 @@ async function responsive() {
           try {
             const result = assetData.join().replace(/<img data-src=['"](.*?)['"](.*?)>/gi, (imgTag, value, alt) => {
               imgTag = imgTag.replace(value, (img) => {
-                if (updatePng[img]) return updatePng[img]['jpg'];
+                if (updatePng[img]) return updatePng[img].jpg;
                 return img;
               });
 
               if (alt.length > 0) {
                 const png = value;
-                const jpg = updatePng[value] ? updatePng[value]['jpg'] : value;
-                const jpg2x = updatePng[value] ? updatePng[value]['jpg2x'] : value;
+                const jpg = updatePng[value] ? updatePng[value].jpg : value;
+                const jpg2x = updatePng[value] ? updatePng[value].jpg2x : value;
 
                 imgTag = imgTag.replace(alt, (postAlt) => {
                   postAlt += ` data-srcset="${jpg}, ${jpg2x} 2x" data-org="${png}" sizes="360px" srcset="${jpg}, ${jpg2x} 2x" src="${jpg}"`;
