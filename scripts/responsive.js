84b16f83d78896a114d8d724dc3e21d80f8b14df
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-09 05:18:31 +0100

    fix: drop file hashing
    - cache busting is not necessary in Cloudfront

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-const { createHash } = require('crypto');
 const { url_for } = require('hexo-util');
 const sharp = require('sharp');
 
@@ -32,18 +31,14 @@ async function responsive() {
               .resize({ width: Math.round(metadata.width / 2) })
               .toBuffer();
 
-            const pngHash = createHash('sha1').update(input).digest('hex').slice(0, 10);
-            const pngPath = path.replace(/\.png$/, `-${pngHash}.png`);
-            const jpeg2xPath = path.replace(/\.png$/, `-${pngHash}@2x.jpg`);
-            const jpegPath = path.replace(/\.png$/, `-${pngHash}.jpg`);
+            const jpeg2xPath = path.replace(/\.png$/, `@2x.jpg`);
+            const jpegPath = path.replace(/\.png$/, `.jpg`);
             const validPath = url_for.call(this, path);
-            const validPngPath = url_for.call(this, pngPath);
             const validJpg2xPath = url_for.call(this, jpeg2xPath);
             const validJpgPath = url_for.call(this, jpegPath);
 
-            updatePng[validPath] = { 'png': validPngPath, 'jpg2x': validJpg2xPath, 'jpg': validJpgPath };
+            updatePng[validPath] = { 'jpg2x': validJpg2xPath, 'jpg': validJpgPath };
 
-            route.set(pngPath, input);
             route.set(jpegPath, halfJpeg);
             resolve(route.set(jpeg2xPath, jpeg));
           } catch (err) {
@@ -62,16 +57,16 @@ async function responsive() {
       assetPath.on('end', async() => {
         if (assetData.length) {
           try {
-            const result = assetData.join().replace(/<img (src|data-src)=['"](.*?)['"](.*?)>/gi, (imgTag, attr, value, alt) => {
+            const result = assetData.join().replace(/<img data-src=['"](.*?)['"](.*?)>/gi, (imgTag, value, alt) => {
               imgTag = imgTag.replace(value, (img) => {
-                if (updatePng[img]) return updatePng[img].png;
+                if (updatePng[img]) return updatePng[img]['jpg'];
                 return img;
               });
 
-              if (attr === 'data-src' && alt.length > 0) {
-                const png = updatePng[value] ? updatePng[value].png : value;
-                const jpg = updatePng[value] ? updatePng[value].jpg : value;
-                const jpg2x = updatePng[value] ? updatePng[value].jpg2x : value;
+              if (alt.length > 0) {
+                const png = value;
+                const jpg = updatePng[value] ? updatePng[value]['jpg'] : value;
+                const jpg2x = updatePng[value] ? updatePng[value]['jpg2x'] : value;
 
                 imgTag = imgTag.replace(alt, (postAlt) => {
                   postAlt += ` data-srcset="${jpg}, ${jpg2x} 2x" data-org="${png}" sizes="360px" srcset="${jpg}, ${jpg2x} 2x" src="${jpg}"`;
