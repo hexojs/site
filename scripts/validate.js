a28884f45ed04177b5a33bab3679d34c5e92d06d
TriDiamond <bennygx@vip.qq.com>
2021-05-01 10:48:42 +0800

    Fixed all eslint errors.

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,63 +1,63 @@
 /* global hexo */
 
-'use strict'
+'use strict';
 
-const { join } = require('path')
+const { join } = require('path');
 
-const { listDir } = require('hexo-fs')
-const { log } = hexo
-const sizeOf = require('image-size')
+const { listDir } = require('hexo-fs');
+const { log } = hexo;
+const sizeOf = require('image-size');
 
 async function validateThemeNames() {
-  let isValidationPassed = true
-  const themeData = hexo.locals.get('data').themes
-  const themeNames = []
-  let name = ''
-  for (let theme of themeData) {
-    name = String(theme.name).toLocaleLowerCase()
+  let isValidationPassed = true;
+  const themeData = hexo.locals.get('data').themes;
+  const themeNames = [];
+  let name = '';
+  for (const theme of themeData) {
+    name = String(theme.name).toLocaleLowerCase();
     if (themeNames.indexOf(name) !== -1) {
-      isValidationPassed = false
-      break
+      isValidationPassed = false;
+      break;
     } else {
-      themeNames.push(name)
+      themeNames.push(name);
     }
   }
 
   if (!isValidationPassed) {
-    throw new Error(`Theme name: [${name}] is duplicated.`)
+    throw new Error(`Theme name: [${name}] is duplicated.`);
   } else {
-    log.info('Theme name validation passed')
+    log.info('Theme name validation passed');
   }
 }
 
 async function validateThemeThumbnail() {
-  let isValidationPassed = true
-  const screenshotsPath = join(hexo.source_dir, 'themes/screenshots')
-  const screenshots = await listDir(screenshotsPath)
+  let isValidationPassed = true;
+  const screenshotsPath = join(hexo.source_dir, 'themes/screenshots');
+  const screenshots = await listDir(screenshotsPath);
 
-  screenshots.forEach((filename) => {
+  screenshots.forEach(filename => {
     if (!filename.endsWith('.png')) {
-      log.fatal(`The theme thumbnail "${filename}" is not a png image.`)
-      isValidationPassed = false
+      log.fatal(`The theme thumbnail "${filename}" is not a png image.`);
+      isValidationPassed = false;
     }
 
-    const screenshot = join(screenshotsPath, filename)
+    const screenshot = join(screenshotsPath, filename);
 
-    const { width, height } = sizeOf(screenshot)
+    const { width, height } = sizeOf(screenshot);
     if (width !== 800 || height !== 500) {
       log.fatal(
         `The theme thumbnail "${filename}" is not sized in 800x500 (got ${width}x${height}).`
-      )
-      isValidationPassed = false
+      );
+      isValidationPassed = false;
     }
-  })
+  });
 
   if (!isValidationPassed) {
-    throw new Error('Theme thumbnails validation failed')
+    throw new Error('Theme thumbnails validation failed');
   } else {
-    log.info('Theme thumbnails validation completed')
+    log.info('Theme thumbnails validation completed');
   }
 }
 
-hexo.extend.filter.register('before_exit', validateThemeNames)
-hexo.extend.filter.register('before_exit', validateThemeThumbnail)
+hexo.extend.filter.register('before_exit', validateThemeNames);
+hexo.extend.filter.register('before_exit', validateThemeThumbnail);
