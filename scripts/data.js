398148cdad96dec3722a149b9eaec3047dc085a3
Mimi <1119186082@qq.com>
2023-07-28 00:15:21 +0800

    separate themes & plugins file

---
+++ scripts/data.js
@@ -0,0 +1,22 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.filter.register('before_generate', async () => {
+  const Data = hexo.model('Data');
+  for (const type of ['themes', 'plugins']) {
+    const arr = [];
+    Data.forEach(({_id, data}) => {
+      if (_id.startsWith(type)) {
+        arr.push({
+          name: _id.replace(type + '/', ''),
+          ...data
+        });
+      }
+    });
+    await Data.insert({
+      _id: type,
+      data: arr
+    });
+  }
+}, 0);
